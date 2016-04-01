a6bec0f81956667e34577844596017da380799dc
Matt Bresnan <bresnan.mw@gmail.com>
2016-03-31 20:47:30 -0700

    organization

--- views/newPhotoView.js
+++ views/newPhotoView.js
@@ -1,9 +1,9 @@
 var NewPhotoView = Backbone.View.extend({
   
 
-  newTemplate: _.template('<form id="newPhoto">New Photo Url: \
-    <input type="text" id="newUrl" name="url" placeholder="Enter Url"><br><br> New Photo Title:\
-    <input type="text" id="newTitle" name="url" placeholder="Enter Title"><br><br> \
+  newTemplate: _.template('<form id="newPhoto">New Photo Title: \
+    <input type="text" id="newTitle" name="url" placeholder="Enter Title"><br><br> New Photo Url:\
+    <input type="text" id="newUrl" name="url" placeholder="Enter Url"><br><br> \
     <input type="submit" value="Submit"></form> '),
 
   
